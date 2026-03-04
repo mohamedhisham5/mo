c80f5f92ddd90f9400e112185f45d57822a9bbf2
mohamedhisham5 <medo6276724@gmail.com>
2026-03-04 16:07:01 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,4 @@
+
+document.getElementById("demo").innerHTML ="Hello World From js code";
+document.getElementById("demo").style.color ="red";
+document.getElementById("demo").style.background ="blue";
